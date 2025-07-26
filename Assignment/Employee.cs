4a94d070a751518c76db1135f4c9eab6fd7ee47f
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-26 20:50:10 +0300

    Question 1

---
+++ Assignment/Employee.cs
@@ -0,0 +1,53 @@
+﻿using Assignment.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    // Question (1)
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        private char gender;
+        public char Gender
+        {
+            get { return gender; }
+            set
+            {
+                if (value == 'M' || value == 'F')
+                    gender = value;
+                else
+                    Console.WriteLine("Gender must be 'M' or 'F'");
+            }
+        }
+        public SecurityLevel Security { get; set; }
+        public double Salary { get; set; }
+        public DateTime HireDate { get; set; }
+
+        public Employee(int id, string name, char gender, SecurityLevel security, double salary, DateTime hireDate)
+        {
+            ID = id;
+            Name = name;
+            Gender = gender;
+            Security = security;
+            Salary = salary;
+            HireDate = hireDate;
+        }
+        public override string ToString()
+        {
+            return $"ID: {ID}\n" +
+                   $"Name: {Name}\n" +
+                   $"Gender: {(Gender == 'M' ? "Male" : "Female")}\n" +
+                   $"Security Level: {Security}\n" +
+                   $"Salary: {Salary}\n" +
+                   $"Hire Date: {HireDate}";
+        }
+
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Assignment
+﻿using Assignment.Enums;
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            // Question (1)
+            Employee emp = new Employee(
+            id: 1,
+            name: "Ahmed Ahmed",
+            gender: 'M',
+            security: SecurityLevel.Developer,
+            salary: 15000,
+            hireDate: new DateTime(2025, 10, 10)
+        );
+
+            Console.WriteLine(emp);
         }
     }
 }
