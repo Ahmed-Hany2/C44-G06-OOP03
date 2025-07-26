2155b127e5fe590a7144db481c8c3673017f1e17
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-26 22:04:05 +0300

    video 3 (inheritance)

---
+++ Demo/Parent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Parent
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Parent(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public virtual int Product()
+        {
+            return X * Y ;
+        }
+
+    }
+}
