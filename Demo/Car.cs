8a7c2a7e423a9dfbdf0c5b517c43827f902c0223
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-26 21:56:38 +0300

    video 2 (class & costructor chaining)

---
+++ Demo/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    #region video 2 (class & costructor chaining)
+    internal class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public int Speed { get; set; }
+        public Car(int id, string model, int speed) : this(id, model)
+        {
+            Id = id;
+            Model = model;
+            Speed = speed;
+        }
+        public Car(int id, string model) :this(id)
+        { 
+            Id = id; 
+            Model = model; 
+        }
+        public Car(int id) 
+        {
+            Id = id; 
+        }
+
+    }
+    #endregion
+}
