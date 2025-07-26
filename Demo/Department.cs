6616395b97eed5e2d4080ae0f21fa6367f183886
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-07-26 22:10:27 +0300

    video 4 (Relationships between classes)

---
+++ Demo/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Department
+    {
+
+    }
+
+    public class University
+    {
+        public List<Department> Department { get; set; }
+    }   
+}
---
+++ Demo/Engine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Engine
+    {
+        public void Start()
+        {
+            Console.WriteLine("Engine started.");
+        }
+    }
+
+    public class Cars
+    {
+        private Engine engine;
+        public Cars() 
+        {
+            engine = new Engine();
+            engine.Start();
+        }
+    }
+}
